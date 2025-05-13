aad2786b4d8cebba939bf7dd2d93d97f589c27d0
Austin McKean <amckean@breakpoint-labs.com>
2025-05-13 15:15:07 -0400

    Added Player Model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import *
+import player
 import pygame
 
 
@@ -10,11 +11,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while 1 == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(SCREEN_COLOR)
+
+        player1.draw(screen)
+
         pygame.display.flip()
         clock.tick(60)
         dt = clock.get_time() / 1000
