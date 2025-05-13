742caf48b570422020809b40097f74a087d304f6
Austin McKean <amckean@breakpoint-labs.com>
2025-05-13 14:01:46 -0400

    Added FPS and Delta Time

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while 1 == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(SCREEN_COLOR)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000
 
 
 if __name__ == "__main__":
